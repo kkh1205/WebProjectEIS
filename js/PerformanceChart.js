7742e46b561172f677060ac7640e16fd4ca47a44
HUNSWORKSPACE\dfg65 <dfg65r@naver.com>
2021-08-23 22:32:45 +0900

    /기훈/ 2021-08-23 마무리 다들 고생많았으

--- js/PerformanceChart.js
+++ js/PerformanceChart.js
@@ -1,41 +1,35 @@
-var ctx = document.getElementById('QualityChart')
+var ctx = document.getElementById('PerformanceChart')
 
-var QualityChart = new Chart(ctx,  {
-    type: 'line',
+var dataset1 = {}
+
+var PerformanceChart = new Chart(ctx,  {
+    type: 'bar',
     data: {
         labels: ['1분기', '2분기', '3분기', '4분기'],
         datasets: [{
             label: 'A-01',
-            data: [0.02, 0.03, 0.09, 0.07],
+            data: [2500, 2000, 3000, 2800],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)'
             ],
-            borderWidth: 1
+            borderWidth: 1,
+            order:1
         },
         {
           label: 'A-02',
-            data: [0.05, 0.02, 0.06, 0.09],
+            data: [3000, 2500, 2500, 3200],
             backgroundColor: [
                 'rgba(54, 162, 235, 0.2)'
             ],
             borderColor: [
                 'rgba(54, 162, 235, 1)'
             ],
-            borderWidth: 1
-        },
-        {
-          label: 'A-03',
-            data: [0.01, 0.04, 0.05, 0.05],
-            backgroundColor: [
-                'rgba(255, 206, 86, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 206, 86, 1)'
-            ],
-            borderWidth: 1
+            borderWidth: 2,
+            type: 'line',
+            order: 0
         }]
     },
     options: {
--- js/mainProduceChart.js
+++ js/mainProduceChart.js
@@ -1,40 +1,48 @@
-var ctx = document.getElementById('ProduceChart').getContext('2d');
-var labels = Utils.months({count: 7});
-var data = {
-  labels: labels,
-  datasets: [{
-    label: 'My First Dataset',
-    data: [65, 59, 80, 81, 56, 55, 40],
-    backgroundColor: [
-      'rgba(255, 99, 132, 0.2)',
-      'rgba(255, 159, 64, 0.2)',
-      'rgba(255, 205, 86, 0.2)',
-      'rgba(75, 192, 192, 0.2)',
-      'rgba(54, 162, 235, 0.2)',
-      'rgba(153, 102, 255, 0.2)',
-      'rgba(201, 203, 207, 0.2)'
-    ],
-    borderColor: [
-      'rgb(255, 99, 132)',
-      'rgb(255, 159, 64)',
-      'rgb(255, 205, 86)',
-      'rgb(75, 192, 192)',
-      'rgb(54, 162, 235)',
-      'rgb(153, 102, 255)',
-      'rgb(201, 203, 207)'
-    ],
-    borderWidth: 1
-  }]
-};
-var ProduceChart = new Chart(ctx, {
-  type: 'bar',
-  data: data,
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  },
-});
+var ctx = document.getElementById('ProduceChart')
 
+var ProduceChart = new Chart(ctx,  {
+    type: 'bar',
+    data: {
+        labels: ['1분기', '2분기', '3분기', '4분기'],
+        datasets: [{
+            label: 'A-01',
+            data: [2500, 2000, 3000, 2800],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)'
+            ],
+            borderWidth: 1
+        },
+        {
+          label: 'A-02',
+            data: [3000, 2500, 2500, 3200],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)'
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+        },
+        {
+          label: 'A-03',
+            data: [1500, 2000, 2500, 2800],
+            backgroundColor: [
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
