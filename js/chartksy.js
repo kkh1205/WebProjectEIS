221e2a21af17ae11b2888228ff1ba608cac9408c
PC-082\user <ksoy0421@gmail.com>
2021-08-27 15:38:37 +0900

    /소연/ 차트 버전 변경 오십퍼 완료

--- js/chartksy.js
+++ js/chartksy.js
@@ -113,13 +113,16 @@ var myChart = new Chart(ctx, {
             display: true,
             title: {
             display: true,
-            text: '만원'
             },
-            
-        }
+            ticks: {
+                // Include a dollar sign in the ticks
+                callback: function(value, index, values) {
+                    return value + '만원'; 
+                }
+            }
         }
     },
-});
+}});
 
 
 
@@ -127,6 +130,55 @@ var myChart = new Chart(ctx, {
 var ctx = document.getElementById('chartsy2').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
+    data: {
+        labels: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+        datasets: [{
+            label: "품질검사",
+            data: [42, 53, 62, 78, 98, 100, 42, 53, 62, 78, 98, 100],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                suggestedMin: 0,
+                suggestedMax: 100,
+                ticks: {
+                    // Include a dollar sign in the ticks
+                    callback: function(value, index, values) {
+                        return value + '%'; 
+                    }
+
+            }
+        }
+    },
+        title: {
+            display: true,
+            text: 'Chart.js Line Chart - Cubic interpolation mode'
+        }
+    }
+});
+
+
+var ctx = document.getElementById('chartsy3').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'doughnut',
     data: {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
@@ -152,10 +204,15 @@ var myChart = new Chart(ctx, {
         }]
     },
     options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Chart.js Doughnut Chart'
+          }
         }
-    }
+    },
 });
