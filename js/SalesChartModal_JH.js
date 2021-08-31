8f06641db17b6f34499090257780c44782ff3c52
PC-080\user <jxs6003@naver.com>
2021-08-31 15:15:13 +0900

    /정현/ 모달창 적용중

--- js/SalesChartModal_JH.js
+++ js/SalesChartModal_JH.js
@@ -1,5 +1,5 @@
-var chart5arr = [2, 3, 4, 1, 2, 3];
-var chart5_1arr = [0,0,0,0,0,0]
+var chart5arr = [100, 40, 50, 40, 20, 30];
+var chart5_1arr = [10,7,3,5,8,2]
 
 
 //A-12 정지사유 차트
@@ -14,6 +14,86 @@ var ctx = document.getElementById("myChart10").getContext('2d');
 
 
 var myChart_10 = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["태성조선", "한화조선", "삼현정밀", "진영기업", "동진조선", "세진기업"],
+        datasets: [
+            {
+                label: '채권회수율(억)',
+                data: chart5arr,
+                backgroundColor: [
+                    
+                    'rgba(54, 162, 235, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,255,255,1)'
+                    
+                ],
+                borderWidth: 1
+            },
+            {
+                label: '미수금(억)',
+                data: chart5_1arr,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)'
+                    
+                ],
+                borderColor: [
+                    'rgba(255,255,255,1)'
+                ],
+                borderWidth: 1
+            }
+        ]
+    },
+    options: {
+        plugins: {
+            legend: {
+                labels: {
+                    usePointStyle: true,
+                    color: "black"
+                }
+            }
+
+        },
+        scales: {
+            X: {
+                ticks: {
+                    color: "black",
+                    fontSize: 10
+                }
+            },
+            Y: {
+                grid: {
+                    drawBorder: false,
+                    color: "gray"
+                    },
+                ticks: {
+                    color: "black",
+                    fontSize: 14,
+                }
+            }
+
+
+        }
+
+
+    }
+});
+
+
+
+
+// 2번째 모달창
+
+var ctx = document.getElementById("myChart11").getContext('2d');
+/*
+- Chart를 생성
+- ctx를 첫번째 argument로 넘겨주고, 
+- 두번째 argument로 그림을 그릴때 필요한 요소들을 모두 넘겨줍
+*/
+
+
+var myChart_11 = new Chart(ctx, {
     type: 'bar',
     data: {
         labels: ["태성조선", "한화조선", "삼현정밀", "진영기업", "동진조선", "세진기업"],
--- js/saleschart_JH.js
+++ js/saleschart_JH.js
@@ -61,8 +61,8 @@ var myChart_1 = new Chart(ctx, {
                 console.log(value);
 
                 
-                var myModal = new bootstrap.Modal(document.getElementById('JanuaryModal'))  //sm. 차트 클릭이벤트와 함께 모달 실행, 모달의 인스턴스 생상하여 모달의 아이디를 추적하여 저장
-                myModal.show()                                                              //sm. 모달 실행
+                var myModal1 = new bootstrap.Modal(document.getElementById('JanuaryModal'))  //sm. 차트 클릭이벤트와 함께 모달 실행, 모달의 인스턴스 생상하여 모달의 아이디를 추적하여 저장
+                myModal1.show()                                                              //sm. 모달 실행
                 
             }
 
@@ -145,6 +145,26 @@ var myChart_2 = new Chart(ctx, {
         }]
     },
     options: {
+
+        onClick: function clickHandler(evt) {           //sm. 클릭으로 이벤트 실행하기 바 차트의 바를 클릭해서 그 바의 값을 가져오고, 모달을 실행하고자함   
+            const points = myChart_2.getElementsAtEventForMode(evt, 'nearest', { intersect: true }, true);
+        
+            if (points.length) {
+                const firstPoint = points[0];   
+                var label = myChart_2.data.labels[firstPoint.index];    //sm. 차트의 바를 클릭했을때 그 라벨명을 가져오기
+                var value = myChart_2.data.datasets[firstPoint.datasetIndex].data[firstPoint.index];    //sm. 차트의 바를 클릭했을때 그 데이터값을 가져오기
+                console.log(label);     //sm. 콘솔로 값이 제대로 가져와지는지 확인
+                console.log(value);
+
+                
+                var myModal2 = new bootstrap.Modal(document.getElementById('productsales'))  //sm. 차트 클릭이벤트와 함께 모달 실행, 모달의 인스턴스 생상하여 모달의 아이디를 추적하여 저장
+                myModal2.show()                                                              //sm. 모달 실행
+                
+            }
+
+            
+        }
+        ,
         
         plugins: {
             legend: {
