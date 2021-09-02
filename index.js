83ca5f83a6ad7ff00e1873bcdfd6db6da500ee90
PC-079\user <e>
2021-09-02 16:23:49 +0900

    /성민/ 차트연동성공

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
-// node_modules 에 있는 express를 require한다
-var express = require('express')
+var express = require('express');
+var app = express();
+
+var options = require('./src/option');
 
 
-// express 는 함수이므로, 반환값을 변수에 저장한다.
-var app = express()
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'database-1.chaokiahnhcd.us-east-2.rds.amazonaws.com',
+  port:3306,
+  user: 'root',
+  password:'123456789',
+  database:'eisprojectdb'
+})
+connection.connect();
 
-//views폴더의 idex파일을 기본 경로로 지정
-app.get('/', function(req,res) {
-    res.sendFile(__dirname + "/public/index.html")
+app.use(express.static('public'));
+
+app.listen(3000,function(){
+  console.log("server start on port 3000!");
 })
 
+app.get('/', function(req,res){
+  res.sendFile(__dirname+'/public/main.html');
+});
 
-//서버의 기본 파일경로를 views폴더로 고정함
-app.use(express.static('public'))
+app.post('/', function(req, res){
+  var responseData = {};
 
+  var query =  connection.query('select score from proUtil', function(err,rows){
+    responseData.score = [];
+    if(err) throw err;
+    responseData.result = "ok";
+    rows.forEach(function(val){
+    responseData.score.push(val.score);
+    })
+    
 
-
-// 3000 포트로 서버 오픈
-app.listen(3000, function() {
-    console.log("로컬호스트 서버 실행 : 3000")
-})
-
-// 이제 터미널에 node index.js 를 입력해보자.
-module.exports = app;
+    res.json(responseData);
+  });
+});
