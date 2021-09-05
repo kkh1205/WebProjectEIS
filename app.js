7f6faaea00eccc19e1dee91380dff36bc3579d5e
PC-082\user <ksoy0421@gmail.com>
2021-09-05 23:39:37 +0900

    /소연/ 데이터가보인다 예에

---
+++ app.js
@@ -0,0 +1,69 @@
+/* KH. 밑의 주석 전부는 김기훈이 작성함. */
+
+var express = require('express'); // HTTP핸들러(서버에 연결하여 DB데이터를 가져오는 기능)를 포함한 웹 프레임워크.
+var app = express(); // express 객체 인스턴스 생성
+var options = require('./src/option'); // fs는 '파일 시스템'의 약자
+// node.js 환경에서 파일시스템에 접근, 사용할 수 있게 해주는 모듈이다.
+
+var mysql = require('mysql'); // mysql 사용시 쓰는 모듈
+// 새 연결 db생성.
+var db = mysql.createConnection({
+  host: 'database-1.chaokiahnhcd.us-east-2.rds.amazonaws.com',
+  port : 3306,
+  user : 'root',
+  password : '123456789',
+  database : 'eisprojectdb'
+})
+
+db.connect(); // 생성된 db 연결
+
+/* 기본라우팅
+라우팅은 URI(또는 경로) 및 특정한 HTTP 요청 메소드(GET, POST 등)인 특정 엔드포인트에 대한 클라이언트 요청에 애플리케이션이 응답하는 방법을 결정하는 것을 말합니다.
+
+각 라우트는 하나 이상의 핸들러 함수를 가질 수 있으며, 이러한 함수는 라우트가 일치할 때 실행됩니다. */
+/* 한줄요약 라우팅 : 사용자의 요청을 적당한 컨트롤러와 연결해주는 작업. */
+
+app.use(express.static('public')); // public을 쓰겠다.
+
+app.listen(3002,function(){
+  console.log("server start on port 3002.");
+}) // 서버를 리스닝 한다. 나는 3001번을 리스닝했다.
+
+app.get('/', function(req,res){ // req(요청),res(응답) '/'는 서버에서의 경로, function은 라우트가 일치할 때 실행되는 함수.
+  res.sendFile(__dirname+'/public/quality.html');
+})
+
+app.post('/', function(req,res){ // 포스트방식으로 데이터쿼리 전송
+  var responseData = {}; // 객체 선언
+
+  var query = db.query('SELECT cost FROM quaYear ', function(err,rows){
+    responseData.cost = [];
+    // responseData.A02 = [];
+    // responseData.A03 = [];
+
+    if(err) throw err;
+    if(rows[0]) {
+      responseData.result = 'ok';
+      rows.forEach(function(val){
+        responseData.cost.push(val.cost);
+      });
+
+    //   rows.forEach(function(val){
+    //     responseData.A02.push(val.A02);
+    //   });
+
+    //   rows.forEach(function(val){
+    //     responseData.A03.push(val.A03);
+    //   });
+    }
+    else{
+      responseData.result = 'none';
+      responseData.cost = '';
+    //   responseData.A02 = '';
+    //   responseData.A03 = '';
+    }
+    res.json(responseData);
+    console.log('success \n'+responseData);
+  });
+});
+
---
+++ public/js/chartksy.js
@@ -0,0 +1,296 @@
+// 검사계획 차트
+// var ctx = document.getElementById('').getContext('2d');
+// var myChart = new Chart(ctx, {
+//     type: 'line',
+//     data: {
+//         labels: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+//         datasets: [{
+//             label: '비용',
+//             lineTension: 0.3,
+//             backgroundColor: "rgba(78, 115, 223, 0.05)",
+//             borderColor: "rgba(78, 115, 223, 1)",
+//             pointRadius: 3,
+//             pointBackgroundColor: "rgba(78, 115, 223, 1)",
+//             pointBorderColor: "rgba(78, 115, 223, 1)",
+//             pointHoverRadius: 3,
+//             pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+//             pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+//             pointHitRadius: 10,
+//             pointBorderWidth: 2,
+//             data: [0, 1000, 500, 1500, 1000, 2000, 1500, 2500, 2000, 3000, 2500, 4000],
+//             fill: true,
+//         }]
+//         },
+//     options: {
+//         responsive: true,
+//         plugins: {
+//         title: {
+//             display: true,
+//             text: 'Chart.js Line Chart - Cubic interpolation mode'
+//         },
+//     },
+//         interaction: {
+//         intersect: false,
+//         },
+//         scales: {
+//         x: {
+//             display: true,
+//             title: {
+//             display: true
+//             }
+//         },
+//         y: {
+//             display: true,
+//             title: {
+//             display: true,
+//             },
+//             ticks: {
+//                 // Include a dollar sign in the ticks
+//                 callback: function(value, index, values) {
+//                     return value + '만원'; 
+//                 }
+//             }
+//         }
+//     },
+// }});
+
+
+
+//품질검사 차트
+var ctx = document.getElementById('chartsy2').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+        datasets: [{
+            label: "품질검사",
+            data: [42, 53, 62, 78, 98, 100, 42, 53, 62, 78, 98, 100],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                suggestedMin: 0,
+                suggestedMax: 100,
+                ticks: {
+                    // Include a dollar sign in the ticks
+                    callback: function(value, index, values) {
+                        return value + '%'; 
+                    }
+
+            }
+        }
+    },
+        title: {
+            display: true,
+            text: 'Chart.js Line Chart - Cubic interpolation mode'
+        }
+    }
+});
+
+
+var ctx = document.getElementById('chartsy3').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1,
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Chart.js Doughnut Chart'
+          }
+        }, 
+        
+    },
+});
+
+var data = {
+            labels: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+            datasets: [{
+            label: '비용',
+            lineTension: 0.3,
+            backgroundColor: "rgba(78, 115, 223, 0.05)",
+            borderColor: "rgba(78, 115, 223, 1)",
+            pointRadius: 3,
+            pointBackgroundColor: "rgba(78, 115, 223, 1)",
+            pointBorderColor: "rgba(78, 115, 223, 1)",
+            pointHoverRadius: 3,
+            pointHoverBackgroundColor: "rgba(78, 115, 223, 1)",
+            pointHoverBorderColor: "rgba(78, 115, 223, 1)",
+            pointHitRadius: 10,
+            pointBorderWidth: 2,
+            data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            fill: true,
+      }
+    ]
+  }
+
+  var data2 = {
+    labels: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+    datasets: [{
+        label: 'A-01',
+        data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)'
+        ],
+        borderWidth: 1
+      }
+    ]
+  }
+
+  var data3 = {
+    labels: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+    datasets: [{
+        label: 'A-01',
+        data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)'
+        ],
+        borderWidth: 1
+      }
+    ]
+  }
+  
+
+var options = {
+    responsive: true,
+    plugins: {
+    title: {
+        display: true,
+        text: 'Chart.js Line Chart - Cubic interpolation mode'
+    },
+},
+    interaction: {
+    intersect: false,
+    },
+    scales: {
+    x: {
+        display: true,
+        title: {
+        display: true
+        }
+    },
+    y: {
+        beginAtZero: true,
+        display: true,
+        title: {
+        display: true,
+        },
+        ticks: {
+            // Include a dollar sign in the ticks
+            callback: function(value, index, values) {
+                return value + '만원'; 
+            }
+        }
+    }
+},
+}
+
+//   var options = {
+//       scales: {
+//         y: {
+//           beginAtZero: true
+//         }
+//       }
+//     }
+  
+  var ctx = document.getElementById('chartsy1').getContext('2d');
+  var ProduceChart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: options
+  });
+
+// 해당 창이 켜질 때, sandAjax를 호출.
+window.onload = function(){
+    sandAjax('http://localhost:3002/');
+  }
+  
+  function sandAjax(url) {
+    var oReq = new XMLHttpRequest(); // XMLHttpRequest객체는 서버와 상호작용 하기위해 사용된다. 새로고침없이도 URL로부터 데이터를 받을 수 있다. 그래서 매개변수를 URL로 받음.
+  
+    oReq.open('POST',url); // url로부터 받는 데이터 형식이 POST라고 선언
+    oReq.setRequestHeader('Content-Type','application/json'); // 컨텐츠 타입을 json이라 알려줌.
+    oReq.send(); // 그걸 보냄
+  
+    oReq.addEventListener('load', function() {
+      var result = JSON.parse(oReq.responseText);
+      console.log(result);
+  
+      var cost = result.cost;
+    //   var A02 = result.A02;
+    //   var A03 = result.A03;
+  
+      var comp_data = data.datasets[0].data;
+    //   var comp_data2 = data1.datasets[1].data;
+    //   var comp_data3 = data1.datasets[2].data;
+  
+      for (var i = 0; i < comp_data.length; i++) {
+        comp_data[i] = cost[i];
+      }
+  
+    //   for (var i = 0; i < comp_data.length; i++) {
+    //     comp_data1[i] = A02[i];
+    //   }
+  
+    //   for (var i = 0; i < comp_data.length; i++) {
+    //     comp_data2[i] = A03[i];
+    //   }
+  
+      data.datasets[0].data = comp_data;
+    //   data1.datasets[1].data = comp_data2;
+    //   data1.datasets[2].data = comp_data3;
+
+      ProduceChart.update();
+    })
+  }
